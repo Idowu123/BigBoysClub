81db40a74fb002f361e86772b3d9aa6af243377d
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2014-06-11 17:15:09 +0100

    change position of next and previous

--- jquery-custom.js
+++ jquery-custom.js
@@ -2,7 +2,6 @@ $( document ).ready(function() {
 	$('#start').click(function(){
 			$('#welcome').fadeOut('fast');
 			$('#car').toggleClass('hide');
-			$('#quiz-nav').toggleClass('hide');
 	});
 
 	$('#next').click(function(){
