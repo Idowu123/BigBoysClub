3a999004d13b09be938eb6fb9a8398714eb3d089
Seun Martins <seun@impactmass.com>
2014-06-12 08:42:59 +0100

    Add jquery next and previous actions

--- jquery-custom.js
+++ jquery-custom.js
@@ -1,10 +1,87 @@
 $( document ).ready(function() {
-	$('#start').click(function(){
-			$('#welcome').fadeOut('fast');
-			$('#car').toggleClass('hide');
+	var pages = ['car', 'watches', 'residence', 'phone', 'club', 'shoe', 'perfume', 'cloth'];
+
+	jQuery.fn.extend({
+		quizNav: function(action){
+			var div = $(this).closest('div');
+			div.toggleClass('hide');
+			if(action === "next"){
+				switch(div.attr('id')){
+					case "car":
+						$("#watch").toggleClass('hide'); 
+						break;
+					case "watch":
+						$("#residence").toggleClass('hide');
+						break;
+					case "residence":
+						$("#phone").toggleClass('hide');
+						break;
+					case "phone":
+						$("#club").toggleClass('hide');
+						break;
+					case "club":
+						$("#shoe").toggleClass('hide');
+						break;
+					case "shoe":
+						$("#perfume").toggleClass('hide');
+						break;
+					case "perfume":
+						$("#cloth").toggleClass('hide');
+						break;
+					case "cloth":
+						//code
+						break;
+					default:
+						// default code
+				}
+			}
+			else if(action === "previous"){
+				switch(div.attr('id')){
+					case "car":
+						//do nothing
+						break;
+					case "watch":
+						$('#car').removeClass('hide');
+						break;
+					case "residence":
+						$("#watch").toggleClass('hide');
+						break;
+					case "phone":
+						$("#residence").toggleClass('hide');
+						break;
+					case "club":
+						$("#phone").toggleClass('hide');
+						break;
+					case "shoe":
+						$("#club").toggleClass('hide');
+						break;
+					case "shoe":
+						$("#cloth").toggleClass('hide');
+						break;
+					case "cloth":
+						$("#shoe").toggleClass('hide');
+						break;
+					default:
+						// default code
+				}
+			}
+		}
 	});
 
-	$('#next').click(function(){
-		
+	$('#start').click(function(event){
+		event.preventDefault();
+		//$(this).closest('div').fadeOut('hide');
+		$('#welcome').fadeOut('slow');
+		$('#car').toggleClass('hide');
 	});
+
+	$('.next').click(function(event){
+		event.preventDefault();
+		$(this).quizNav("next");		
+	});
+
+	$('.previous').click(function(event){
+		event.preventDefault();
+		$(this).quizNav("previous");		
+	});  
 });
